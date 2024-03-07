9f13104657b18c4804cbb76fc4dbdc3dac593352
Pedro Viera <126673608+PedroVieira0406@users.noreply.github.com>
2024-03-07 09:12:41 -0300

    Update Main.java

--- PV_01/src/aula05/Main.java
+++ PV_01/src/aula05/Main.java
@@ -3,7 +3,7 @@ package aula05;
 public class Main {
 
 	public static void main(String[] args) {
-		PilhaDinamica pilha = new PilhaDinamica();
+		PilhaDinamica<Integer> pilha = new PilhaDinamica<Integer>();
 		pilha.push(20);
 		pilha.push(30);
 		pilha.push(40);
@@ -13,7 +13,7 @@ public class Main {
 		pilha.imprimir();
 		System.out.println("----");
 		
-		FilaDinamica f = new FilaDinamica();
+		FilaDinamica<Integer> f = new FilaDinamica<Integer>();
 		f.push(20);
 		f.push(30);
 		f.push(40);
